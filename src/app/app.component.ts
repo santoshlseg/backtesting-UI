364dcd5aa1e467c9e1e2fea1bde73f8d74cab1bd
Santosh <santosh.kumarnayak.rdp@lseg.com>
2022-10-18 23:30:30 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import sampleData from '../assets/data.json';
+import performanceData from '../assets/performance.json';
+import priceData from '../assets/prices.json';
+
 
 import '@elf/amber-loader';
 import '@elf/carbon-sidebar-layout';
@@ -50,7 +53,22 @@ import { ParseFlags } from '@angular/compiler';
 export class AppComponent {
 loading: boolean = false;
 title = 'backtestingUI';
-Users: any = sampleData;
+Users: any = sampleData;            // Read from Jason file 
+perf :any =performanceData;
+price :any =priceData;
+
+universe: Array<any> = [
+  { label: 'Singapore', value: 'SG' },
+  { label: 'China', value: 'CN' },
+  { label: 'Hong Kong', value: 'HK' },
+  { label: 'Taiwan', value: 'TW' },
+  { label: 'Thailand', value: 'TH' , selected: true},
+  { label: 'Canada', value: 'CA' },
+  { label: 'India', value: 'IN' },
+  { label: 'United States', value: 'US' }
+];
+
+gridData1: Array<any> = [];
 
 portfolios : any;
 performance : any;
@@ -63,56 +81,109 @@ toDate : any;
 startDate1 : any;
 endDate1 : any;
 json_Data : any = [];
-display_Data :any = [];
+json_Data1 : any = [];
+userSelectedDateRange :any = [];
 data : any = [];
+
+start_Date :any = '2022-10-01';          // Initialized to Dummy values
+end_Date : any = '2022-10-30';
+series_Data : any = [
+  {
+    symbol: 'GOOGL.O',
+    type: 'line',
+    //data : this.display_Data
+    //data : this.generateChart('2022-10-01', '2022-10-30')
+    data : this.generateChart(this.start_Date, this.end_Date)
+  },
+  {
+    symbol: 'AMZN.OQ',
+    type: 'line',
+    //data : this.generateChart('2022-10-01', '2022-10-30')
+    data: this.generateChart(this.start_Date, this.end_Date)
+  },
+  {
+    symbol: 'WALMART.OQ',
+    type: 'line',
+    //data : this.generateChart('2022-10-01', '2022-10-30')
+    data: this.generateChart(this.start_Date, this.end_Date)
+  },
+  {
+    symbol: 'DISNEY.OQ',
+    type: 'line',
+    //data : this.generateChart('2022-10-01', '2022-10-30')
+    data: this.generateChart(this.start_Date, this.end_Date)
+  }
+];
 
 constructor(){}
   
 ngOnInit(): void {  
     this.uploadJsonDatatoArray();
-     
-    //this.generateTable();     
-    this.initializeDemo();
-    this.renderResultGrid("Hello");
+    this.onSelectFromDate(); 
+    this.onSelectToDate(); 
+    this.onSubmit();
+    this.initializeUniverseComboBox();           
+    this.initializeLayoutEvent();
+    this.readJsonDatatoArray();
+    this.onSelectSignal();
   }                    
 
+
+  initializeUniverseComboBox(): void {
+    const comboBox = document.getElementById('selectUniverseComboBox') as any;
+    comboBox.data = this.universe;
+    comboBox.addEventListener('value-changed', (event: any) => {
+       this.universeComboBoxChanged(event);
+     });
+  }
+
+  universeComboBoxChanged(event: any): void {
+    this.universe = event.detail.value;
+    console.log("univserse" +this.universe);
+  }
+
   uploadJsonDatatoArray(): void {
-    for(var i=0;i< 6;i++) {
+    for(var i=0;i<6;i++) {
       //console.log(this.getTime(i));
       //console.log(this.getValue(i));
-      this.json_Data.push({ time: this.getTime(i),value : this.getValue(i)});
+      this.json_Data.push({ time: this.Users[i]["time"],value : this.Users[i]["phone"]});
       //console.log("count="+this.json_Data.length);
     }
   }
 
-  initializeDemo(): void {
-    this.initializeLayoutEvent();
+  readJsonDatatoArray(): void {
+    for(var i=0;i<6;i++) {
+      this.json_Data1.push({ time: this.Users[i]["time"], 
+      name : this.Users[i]["name"],
+      phone : this.Users[i]["phone"], 
+      country: this.Users[i]["country"],
+      frequency : this.Users[i]["frequency"],
+      return : this.Users[i]["return"],
+      cagr : this.Users[i]["cagr"],
+      volatility : this.Users[i]["volatility"],
+      ratio : this.Users[i]["ratio"]
+    });
+    }
   }
 
-
   onSubmit() : void {    
-    console.log("submit button has been clicked");
-    // 1)Validate the User Input Date 
     var isProperDates :boolean = this.validateDate(this.fromDate,this.toDate);
-    //console.log("Proper Dates =" + this.fromDate);
-    //console.log("Proper Dates =" + this.toDate);
-    //console.log("Proper Dates =" +isProperDates);
-    
-    // 2)Retrive the date range 
     if(isProperDates==true){
       this.calculateDateRangeForGraph(this.fromDate,this.toDate);
     }
-
-    // 3)Chart generation code
+    else{
+      alert("Provide proper data range");
+    }
     var n : any = document.getElementById("selectN") as any;
-    //console.log(n.value);
     for(var i=0;i< n.value ;i++) {
       //console.log(this.getTime(i));
       //console.log(this.getValue(i));
-      this.display_Data.push({ time: this.getTime(i),value : this.getValue(i)});
+      this.userSelectedDateRange.push({ time: this.Users[i]["time"],value : this.Users[i]["phone"]});
     }
       setTimeout(()=> this.displayMultipleChart(this.fromDate,this.toDate),1000);
       this.readJsonFile();  
+      this.renderPerformanceGrid();
+      this.renderResultGrid();
   }
 
   initializeLayoutEvent(): void {
@@ -124,28 +195,23 @@ ngOnInit(): void {
     });
   }
     
-  getTime( j: number): any {
-    var time = (this.Users[j]["time"]);
-    return time;
-} 
-
-  getValue( j: number): any {
-    var phone = (this.Users[j]["phone"]);
-    return phone;
-} 
+//   getTime( j: number): any {
+//     var time = (this.Users[j]["time"]);
+//     return time;
+// } 
+//   getValue( j: number): any {
+//     var phone = (this.Users[j]["phone"]);
+//     return phone;
+// } 
 
   onSelectFromDate(){
     var temp_fromdate =document.getElementById("from-date") as any;
     this.fromDate=temp_fromdate.value;
-    //console.log("Date from UI =" +this.fromDate);
   }
-
   onSelectToDate(){
     var temp_todate=document.getElementById("to-date") as any;
     this.toDate=temp_todate.value;
-    //console.log("Date from UI= "+this.toDate);
   }
-
   validateDate(fromDate : string, toDate : string) : boolean{
     const startDate = new Date(fromDate);
     const endDate = new Date(toDate);
@@ -154,73 +220,116 @@ ngOnInit(): void {
       alert("startDate date > end Date");
     }
     else if(startDate < endDate){ 
-      //console.log("startDate date < end Date");
       flag=true;
     }
     else{
       console.log("Both dates are same");
     }
-    //console.log(flag);
     return flag;
   }
 
-
   calculateDateRangeForGraph(fromDate : string, toDate : string) : any{
-    //console.log("Inside calculateDateRangeForGraph");
-    //console.log("fromDate" +fromDate);
-    //console.log("toDate" +toDate);
-    //console.log(this.json_Data.length);
-
     for(var i=0;i< this.json_Data.length ;i++){
       var temp : any = this.json_Data[i].time;
       if(temp >= fromDate && temp <= toDate){
-        //console.log("temp date="+temp);
-        //console.log("fromDate ="+fromDate);
-        //console.log("toDate="+ toDate);
-        this.display_Data.push(temp);
+        this.userSelectedDateRange.push(temp);
       }
-      //console.log("Display data length= " + this.display_Data.length);
     }
-    return this.display_Data;
+    return this.userSelectedDateRange;
+  }
+
+  onSelectSignal(){
+    var signal=document.getElementById("selectSignal") as any;
+    console.log("signal =" +signal.value);
   }
 
 
-  renderResultGrid(instrument : string) {
-      //alert("renderResultGrid");
-      
-      var fields = ['intCol', 'strCol', 'floatCol'];
+  renderPerformanceGrid() {
+    this.loading = true;
+    var fields = ['strCol1','strCol2','strCol3','strCol4', 'strCol5','strCol6','floatCol'];
+    var grid = document.getElementById('grid1') as any;
+    var temp_fromdate =document.getElementById("from-date") as any;
+    this.fromDate=temp_fromdate.value;
+    var temp_todate=document.getElementById("to-date") as any;
+    this.toDate=temp_todate.value;
+    var signal = document.getElementById('selectSignal') as any;
+    var benchMark = document.getElementById('benchMark') as any;
+    var spread = document.getElementById('spread') as any;
+
+    const gridData2 = [
+      ["Start Date",this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate
+      ,this.fromDate],
+      ["End Date",this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate],
+      ["Signal",signal.value,signal.value,signal.value,signal.value,signal.value,spread.value,benchMark.value]
+      ] as any;
+
+    grid.config = {
+      rowHeight: 40,
+    sorting: {
+    sortableColumns: true
+    },
+    columns: [
+    { title: 'Metric', field: fields[0] },
+    { title: 'Q1', field: fields[1] },
+    { title: 'Q2', field: fields[2] },
+    { title: 'Q3', field: fields[3] },
+    { title: 'Q4', field: fields[4] },
+    { title: 'Q5', field: fields[5] },
+    { title: 'Long/Short Spread', field: fields[6]},
+    { title: 'BenchMark', field: fields[7] },
+  ],
+  dataModel: { 
+  fields: fields,
+  data : gridData2
+   } };
+}
+
+
+
+  renderResultGrid() {
+      //console.log("renderResultGrid");
+      this.loading = true;
+      var fields = ['strCol1','strCol2','strCol3','strCol4','strCol5','strCol6','strCol7', 'strCol8','strCol9' ];
       var grid = document.getElementById('grid') as any;
+
+      const gridData = [] as any;
+
+      for(var i=0;i<this.json_Data1.length;i++){
+        console.log("value of i" +i);
+        gridData.push([this.json_Data1[i].time,
+          this.json_Data1[i].name, 
+          this.json_Data1[i].phone,
+          this.json_Data1[i].country,
+          this.json_Data1[i].frequency,
+          this.json_Data1[i].return,
+          this.json_Data1[i].cagr,
+          this.json_Data1[i].volatility,
+          this.json_Data1[i].ratio
+          
+        ]);
+        //console.log(this.json_Data1[i].frequency);
+      }  
+      
       grid.config = {
+        rowHeight: 40,
       sorting: {
       sortableColumns: true
       },
-      columns: [
-      { title: 'portfolio', field: fields[0], width: 70 },
-      { title: 'Total Number of Observations', field: fields[1], minWidth: 55 },
-      { title: 'Valid Observations', field: fields[2] },
-      { title: 'Start Date', field: fields[3] },
-      { title: 'End Date', field: fields[4] },
-      { title: 'Frequency', field: fields[5], width: 60, },
-      { title: 'Cumulative Return', field: fields[6] },
-      { title: 'CAGR', field: fields[7] },
-      { title: 'Annualized Volatility', field: fields[8] },
-      { title: 'Information Ratio', field: fields[9] },
-      { title: 't-stat', field: fields[10] },
-      { title: 'Skewness', field: fields[11] },
-      { title: 'Kurtosis', field: fields[12] },
-      { title: 'Hit Ratio', field: fields[13] },
-      { title: 'Maximum Drawdown', field: fields[14] },
 
+      columns: [
+      { title: 'Start Date', field: fields[0] },
+      { title: 'Name of Observation', field: fields[1] },
+      { title: 'Valid Observations', field: fields[2] },
+      { title: 'Country', field: fields[3] , visible :true },
+      { title: 'Frequency', field: fields[4] , visible :true },
+       { title: 'Cummulative Return', field: fields[5] , visible :true },
+       { title: 'CAGR', field: fields[6] , visible :true },
+       { title: 'Annualized Volatility', field: fields[7] , visible :true },
+       { title: 'Information Ratio', field: fields[8] , visible :true },
     ],
     dataModel: { 
     fields: fields,
-    data: [
-      [1, 'Singapore', 5.50, 111 ],
-      [2, 'HongKong', 7.00, 10],
-      [3, 'Venice', 25.00, 100],
-      [4, 'Sri Lanka', 20.00, 222],
-      [5, 'Malaysia', 5.75, 333]
-    ]
+    data : gridData
      } };
 }
 
@@ -229,8 +338,6 @@ generateChart(from: string, to: string) {
   let initVal = 20;
   const startDate = new Date(from);
   const endDate = new Date(to);
-  //console.log("startDate generateChart=="+ startDate);
-  //console.log("endDate generateChart=="+ endDate);
   const ret = [];
   const total = Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
   for (let i = 0; i < total; i++) {
@@ -247,7 +354,6 @@ generateChart(from: string, to: string) {
   return ret;
 }
 
-     
 displayMultipleChart(fromDate1 : string, toDate1 : string): any {
      const startDate1 = new Date(fromDate1);
      const endDate1 = new Date(toDate1);
@@ -257,66 +363,54 @@ displayMultipleChart(fromDate1 : string, toDate1 : string): any {
      var endDt :string = endDate1.toString();
      var multiplechart : any = document.getElementById("multiple") as any;
      var selectN : any = document.getElementById("selectN") as any;
-     console.log("Multiple Chart value of selectN " + selectN.value);
 
+     var temp_series :any = [];
+    //  this.start_Date = startDt;
+    //  this.end_Date = endDt;
+    this.start_Date = startDate1;
+    this.end_Date = endDate1;
+    //  console.log("startDt="+startDt);
+    //  console.log("endDt="+ endDt);
+    //  console.log("startDate="+this.start_Date);
+    //  console.log("endDate="+this.end_Date);
+
+     for(var i= 0;i< selectN.value;i++){
+       temp_series.push(this.series_Data[i]);
+     }
     multiplechart.config = {
       options: {
         priceScale: {
           mode: 2
         },
       },  
-      series: [
-        {
-          symbol: 'GOOGL.O',
-          type: 'line',
-          //data : this.display_Data
-          //data : this.generateChart('2020-09-08', '2022-09-10')
-          data : this.generateChart(startDt, endDt)
-        },
-        {
-          symbol: 'AMZN.OQ',
-          type: 'line',
-          data: this.generateChart(startDt, endDt)
-        },
-        {
-          symbol: 'WALMART.OQ',
-          type: 'line',
-          data: this.generateChart(startDt, endDt)
-        },
-        {
-          symbol: 'DISNEY.OQ',
-          type: 'line',
-          data: this.generateChart(startDt, endDt)
-        }
-      ]
+      series: temp_series
     }
   }
 
-  // which takes n as input 
+  
+   retrieveData() : any {
+     this.loading = true;
+     console.log("Loading data");
+     return fetch(`assets/data.json`)
+       .then((response) => response.json())
+       .then((data) => {
+         this.loading = false;
+         console.log("data ="+ data);
+         return data;
+      });
+   }
 
-  sizeofLoop(sizeN : number ){
-    // Data of series will come here and ietrate as per value of n 
 
-
-
-
-
-
-  }
-
-
-
-
-   readJsonFile() :  any{
-    console.log("Inside readJson method :");
+   readJsonFile() :  any {
+    //console.log("Inside readJson method :");
     this.loading = true;
     return fetch(`assets/performance.json`)
       .then((response) => response.json())
       .then((data) => {
         this.loading = false;
-        console.log("Json data :"+ data.time);
-        console.log("Json data :"+ data.phone);
-        console.log("Json data :"+ data.size);
+        //console.log("Json data :"+ data.time);
+        //console.log("Json data :"+ data.phone);
+        //console.log("Json data :"+ data.size);
         return data;
       });
   }
