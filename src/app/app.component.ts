f52dbb0b61bc0b860ba51db91d3e2fd185b32361
Santosh <santosh.kumarnayak.rdp@lseg.com>
2022-10-20 20:04:25 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,7 @@ import '@elf/quartz-layout';
 import '@elf/sapphire-bar';
 import '@elf/sapphire-interactive-chart';
 import '@elf/coral-search-field';
+import '@elf/coral-toggle';
 
 import '@elf/elf-theme-halo/light/imports/native-elements';
 import '@elf/amber-loader/themes/halo/light';
@@ -51,11 +52,8 @@ import { ParseFlags } from '@angular/compiler';
 })
 
 export class AppComponent {
-loading: boolean = false;
+//loading: boolean = false;
 title = 'backtestingUI';
-Users: any = sampleData;            // Read from Jason file 
-perf :any =performanceData;
-price :any =priceData;
 
 universe: Array<any> = [
   { label: 'Singapore', value: 'SG' },
@@ -76,58 +74,49 @@ dateFormat : any;
 decimalPlace :any;
 metrics:any;
 
-fromDate : any;
+fromDate : any ; 
 toDate : any;
 startDate1 : any;
 endDate1 : any;
 json_Data : any = [];
 json_Data1 : any = [];
-userSelectedDateRange :any = [];
 data : any = [];
 
-start_Date :any = '2022-10-01';          // Initialized to Dummy values
-end_Date : any = '2022-10-30';
+start_Date :any = '2022-01-01';          
+end_Date : any = '2022-12-31';
+
 series_Data : any = [
   {
-    symbol: 'GOOGL.O',
+    symbol: 'Q1',
     type: 'line',
-    //data : this.display_Data
-    //data : this.generateChart('2022-10-01', '2022-10-30')
-    data : this.generateChart(this.start_Date, this.end_Date)
+    data : this.generateChart()
   },
   {
-    symbol: 'AMZN.OQ',
+    symbol: 'Q2',
     type: 'line',
-    //data : this.generateChart('2022-10-01', '2022-10-30')
-    data: this.generateChart(this.start_Date, this.end_Date)
+    data: this.generateChart()
   },
   {
-    symbol: 'WALMART.OQ',
+    symbol: 'Q3',
     type: 'line',
-    //data : this.generateChart('2022-10-01', '2022-10-30')
-    data: this.generateChart(this.start_Date, this.end_Date)
+    data: this.generateChart()
   },
   {
-    symbol: 'DISNEY.OQ',
+    symbol: 'Q4',
     type: 'line',
-    //data : this.generateChart('2022-10-01', '2022-10-30')
-    data: this.generateChart(this.start_Date, this.end_Date)
+    data: this.generateChart()
   }
 ];
 
 constructor(){}
   
 ngOnInit(): void {  
-    this.uploadJsonDatatoArray();
-    this.onSelectFromDate(); 
-    this.onSelectToDate(); 
+  //this.loading = false;
     this.onSubmit();
     this.initializeUniverseComboBox();           
     this.initializeLayoutEvent();
-    this.readJsonDatatoArray();
     this.onSelectSignal();
   }                    
-
 
   initializeUniverseComboBox(): void {
     const comboBox = document.getElementById('selectUniverseComboBox') as any;
@@ -142,48 +131,20 @@ ngOnInit(): void {
     console.log("univserse" +this.universe);
   }
 
-  uploadJsonDatatoArray(): void {
-    for(var i=0;i<6;i++) {
-      //console.log(this.getTime(i));
-      //console.log(this.getValue(i));
-      this.json_Data.push({ time: this.Users[i]["time"],value : this.Users[i]["phone"]});
-      //console.log("count="+this.json_Data.length);
-    }
-  }
-
-  readJsonDatatoArray(): void {
-    for(var i=0;i<6;i++) {
-      this.json_Data1.push({ time: this.Users[i]["time"], 
-      name : this.Users[i]["name"],
-      phone : this.Users[i]["phone"], 
-      country: this.Users[i]["country"],
-      frequency : this.Users[i]["frequency"],
-      return : this.Users[i]["return"],
-      cagr : this.Users[i]["cagr"],
-      volatility : this.Users[i]["volatility"],
-      ratio : this.Users[i]["ratio"]
-    });
-    }
-  }
-
-  onSubmit() : void {    
+  onSubmit() : void {  
+    this.onSelectFromDate(); 
+    this.onSelectToDate();   
     var isProperDates :boolean = this.validateDate(this.fromDate,this.toDate);
-    if(isProperDates==true){
-      this.calculateDateRangeForGraph(this.fromDate,this.toDate);
-    }
-    else{
+    if(isProperDates==false){
       alert("Provide proper data range");
     }
-    var n : any = document.getElementById("selectN") as any;
-    for(var i=0;i< n.value ;i++) {
-      //console.log(this.getTime(i));
-      //console.log(this.getValue(i));
-      this.userSelectedDateRange.push({ time: this.Users[i]["time"],value : this.Users[i]["phone"]});
-    }
-      setTimeout(()=> this.displayMultipleChart(this.fromDate,this.toDate),1000);
-      this.readJsonFile();  
-      this.renderPerformanceGrid();
-      this.renderResultGrid();
+    var temp_fromdate =document.getElementById("from-date") as any;
+    this.fromDate=temp_fromdate.value;
+    var temp_todate=document.getElementById("to-date") as any;
+    this.toDate=temp_todate.value;
+    this.displayMultipleChart(this.fromDate,this.toDate);
+    this.renderPerformanceGrid();
+    //this.generateChart();
   }
 
   initializeLayoutEvent(): void {
@@ -195,18 +156,9 @@ ngOnInit(): void {
     });
   }
     
-//   getTime( j: number): any {
-//     var time = (this.Users[j]["time"]);
-//     return time;
-// } 
-//   getValue( j: number): any {
-//     var phone = (this.Users[j]["phone"]);
-//     return phone;
-// } 
-
   onSelectFromDate(){
     var temp_fromdate =document.getElementById("from-date") as any;
-    this.fromDate=temp_fromdate.value;
+    this.fromDate = temp_fromdate.value;
   }
   onSelectToDate(){
     var temp_todate=document.getElementById("to-date") as any;
@@ -228,39 +180,42 @@ ngOnInit(): void {
     return flag;
   }
 
-  calculateDateRangeForGraph(fromDate : string, toDate : string) : any{
-    for(var i=0;i< this.json_Data.length ;i++){
-      var temp : any = this.json_Data[i].time;
-      if(temp >= fromDate && temp <= toDate){
-        this.userSelectedDateRange.push(temp);
-      }
-    }
-    return this.userSelectedDateRange;
-  }
-
   onSelectSignal(){
     var signal=document.getElementById("selectSignal") as any;
     console.log("signal =" +signal.value);
   }
 
-
   renderPerformanceGrid() {
-    this.loading = true;
-    var fields = ['strCol1','strCol2','strCol3','strCol4', 'strCol5','strCol6','floatCol'];
+    var fields = ['strCol1','strCol2','strCol3','strCol4', 'strCol5','strCol6','floatCol',
+    'strCol7','strCol8'];
     var grid = document.getElementById('grid1') as any;
     var temp_fromdate =document.getElementById("from-date") as any;
     this.fromDate=temp_fromdate.value;
     var temp_todate=document.getElementById("to-date") as any;
     this.toDate=temp_todate.value;
     var signal = document.getElementById('selectSignal') as any;
-    var benchMark = document.getElementById('benchMark') as any;
+    //var benchMark = document.getElementById('benchMark') as any;
     var spread = document.getElementById('spread') as any;
 
-    const gridData2 = [
+    const performanceGrid = [
       ["Start Date",this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate
       ,this.fromDate],
       ["End Date",this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate],
-      ["Signal",signal.value,signal.value,signal.value,signal.value,signal.value,spread.value,benchMark.value]
+      ["Signal",signal.value,signal.value,signal.value,signal.value,signal.value,spread.value,999],
+      ["Total Number of Observations","148","148","148","148","148","148","148"],
+      ["Valid Observations","147","147","147","147","147","147","147"],
+      ["Cumulative Returns", "0.019","0.037","0.084","0.028","0.035","0.012","0.021"],
+      ["CAGR","0.002","0.003","0.007","0.002","0.003","0.001","0.009"],
+      ["Annualized Volatility","0.013","0.015","0.012","0.013","0.015","0.012","0.013"],
+      ["Information Ratio","0.118","0.204","0.553","0.118","0.204","0.553","0.118"],
+      ["t-stat","0.118","0.204","0.553","0.118","0.204","0.553","0.118"],
+      ["Skewness","0.118","0.204","0.553","0.225","0.204","0.553","0.118"],
+      ["Kurtosis","0.555","0.286","-0.047","1.556","0.411","0.437","-0.148"],
+      ["Hit Ratio","0.517","0.469","0.551","0.524","0.49","0.524","0.51"],
+      // [Maximum Drawdown],
+      // [Value-at-Risk 95%],
+      // [Expected Shortfall 95],
+      // [Turnover]
       ] as any;
 
     grid.config = {
@@ -269,113 +224,54 @@ ngOnInit(): void {
     sortableColumns: true
     },
     columns: [
-    { title: 'Metric', field: fields[0] },
+    { title: 'Metric', field: fields[0] , width: 300},
     { title: 'Q1', field: fields[1] },
     { title: 'Q2', field: fields[2] },
     { title: 'Q3', field: fields[3] },
     { title: 'Q4', field: fields[4] },
     { title: 'Q5', field: fields[5] },
     { title: 'Long/Short Spread', field: fields[6]},
-    { title: 'BenchMark', field: fields[7] },
+    { title: 'CSI300', field: fields[7] },
   ],
   dataModel: { 
   fields: fields,
-  data : gridData2
+  data : performanceGrid
    } };
 }
 
 
-
-  renderResultGrid() {
-      //console.log("renderResultGrid");
-      this.loading = true;
-      var fields = ['strCol1','strCol2','strCol3','strCol4','strCol5','strCol6','strCol7', 'strCol8','strCol9' ];
-      var grid = document.getElementById('grid') as any;
-
-      const gridData = [] as any;
-
-      for(var i=0;i<this.json_Data1.length;i++){
-        console.log("value of i" +i);
-        gridData.push([this.json_Data1[i].time,
-          this.json_Data1[i].name, 
-          this.json_Data1[i].phone,
-          this.json_Data1[i].country,
-          this.json_Data1[i].frequency,
-          this.json_Data1[i].return,
-          this.json_Data1[i].cagr,
-          this.json_Data1[i].volatility,
-          this.json_Data1[i].ratio
-          
-        ]);
-        //console.log(this.json_Data1[i].frequency);
-      }  
-      
-      grid.config = {
-        rowHeight: 40,
-      sorting: {
-      sortableColumns: true
-      },
-
-      columns: [
-      { title: 'Start Date', field: fields[0] },
-      { title: 'Name of Observation', field: fields[1] },
-      { title: 'Valid Observations', field: fields[2] },
-      { title: 'Country', field: fields[3] , visible :true },
-      { title: 'Frequency', field: fields[4] , visible :true },
-       { title: 'Cummulative Return', field: fields[5] , visible :true },
-       { title: 'CAGR', field: fields[6] , visible :true },
-       { title: 'Annualized Volatility', field: fields[7] , visible :true },
-       { title: 'Information Ratio', field: fields[8] , visible :true },
-    ],
-    dataModel: { 
-    fields: fields,
-    data : gridData
-     } };
-}
-
-
-generateChart(from: string, to: string) {            
-  let initVal = 20;
-  const startDate = new Date(from);
-  const endDate = new Date(to);
-  const ret = [];
-  const total = Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
-  for (let i = 0; i < total; i++) {
-      const volatility = (Math.random() * (4.5) - 2) / 100; // random % volatility
-      const date = new Date(startDate.setDate(startDate.getDate() + 1));
-      const val = initVal + initVal * volatility;
-      initVal = val;
-      const point = {
-          time: date.getTime() / 1000.0,
-          value: parseFloat(val.toFixed(2))
-      };
-      ret.push(point);
-  }
-  return ret;
-}
-
 displayMultipleChart(fromDate1 : string, toDate1 : string): any {
      const startDate1 = new Date(fromDate1);
      const endDate1 = new Date(toDate1);
-     //console.log("const startDate"+ startDate1);
-     //console.log("endDate"+ endDate1);
-     var startDt :string = startDate1.toString();
-     var endDt :string = endDate1.toString();
      var multiplechart : any = document.getElementById("multiple") as any;
-     var selectN : any = document.getElementById("selectN") as any;
-
-     var temp_series :any = [];
-    //  this.start_Date = startDt;
-    //  this.end_Date = endDt;
+     var selectFractile : any = document.getElementById("selectFractile") as any;
+    var temp_series :any = [];
+    var fractile_data : any =[
+      {
+        symbol: 'Q1',
+        type: 'line',
+        data : this.generateChart()
+      },
+      {
+        symbol: 'Q2',
+        type: 'line',
+        data: this.generateChart()
+      },
+      {
+        symbol: 'Q3',
+        type: 'line',
+        data: this.generateChart()
+      },
+      {
+        symbol: 'Q4',
+        type: 'line',
+        data: this.generateChart()
+      }
+    ];
     this.start_Date = startDate1;
     this.end_Date = endDate1;
-    //  console.log("startDt="+startDt);
-    //  console.log("endDt="+ endDt);
-    //  console.log("startDate="+this.start_Date);
-    //  console.log("endDate="+this.end_Date);
-
-     for(var i= 0;i< selectN.value;i++){
-       temp_series.push(this.series_Data[i]);
+     for(var i= 0;i< selectFractile.value;i++){
+       temp_series.push(fractile_data[i]);
      }
     multiplechart.config = {
       options: {
@@ -383,36 +279,28 @@ displayMultipleChart(fromDate1 : string, toDate1 : string): any {
           mode: 2
         },
       },  
-      series: temp_series
+      series:temp_series
     }
   }
-
-  
-   retrieveData() : any {
-     this.loading = true;
-     console.log("Loading data");
-     return fetch(`assets/data.json`)
-       .then((response) => response.json())
-       .then((data) => {
-         this.loading = false;
-         console.log("data ="+ data);
-         return data;
-      });
-   }
-
-
-   readJsonFile() :  any {
-    //console.log("Inside readJson method :");
-    this.loading = true;
-    return fetch(`assets/performance.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        this.loading = false;
-        //console.log("Json data :"+ data.time);
-        //console.log("Json data :"+ data.phone);
-        //console.log("Json data :"+ data.size);
-        return data;
-      });
+   
+  generateChart() {            
+    let initVal = 20;
+     const startDate = new Date(this.start_Date);
+     const endDate = new Date(this.end_Date);
+    const ret = [];
+    const total = Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
+    for (let i = 0; i < total; i++) {
+        const volatility = (Math.random() * (4.5) - 2) / 100; // random % volatility
+        const date = new Date(startDate.setDate(startDate.getDate() + 1));
+        const val = initVal + initVal * volatility;
+        initVal = val;
+        const point = {
+            time: date.getTime() / 1000.0,
+            value: parseFloat(val.toFixed(2))
+        };
+        ret.push(point);
+    }
+    return ret;
   }
 
 }
