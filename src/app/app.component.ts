57519a2f8816ae0797a7604ec2493cb1b8e04420
Santosh <santosh.kumarnayak.rdp@lseg.com>
2022-10-27 20:41:01 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,9 +42,7 @@ import '@elf/emerald-multi-select/themes/halo/light';
 import '@elf/quartz-layout/themes/halo/light';
 import '@elf/sapphire-bar/themes/halo/light';
 import '@elf/sapphire-interactive-chart/themes/halo/light';
-import { formatDate, Time } from '@angular/common';
-import { ParseFlags } from '@angular/compiler';
-import { AUTO_STYLE } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
@@ -65,12 +63,11 @@ universe: Array<any> = [
   { label: 'United States', value: 'US' }
 ];
 
-gridData1: Array<any> = [];
 
-portfolios : any;
+//portfolios : any;
 performance : any;
 dateFormat : any;
-metrics:any;
+//metrics:any;
 
 fromDate : any ; 
 toDate : any;
@@ -79,32 +76,12 @@ endDate1 : any;
 json_Data : any = [];
 json_Data1 : any = [];
 data : any = [];
+columnArray : Array<any> = [];
+observationArray : any =[];
 
 start_Date :any = '2022-01-01';          
 end_Date : any = '2022-12-31';
 
-series_Data : any = [
-  {
-    symbol: 'Q1',
-    type: 'line',
-    data : this.generateChart()
-  },
-  {
-    symbol: 'Q2',
-    type: 'line',
-    data: this.generateChart()
-  },
-  {
-    symbol: 'Q3',
-    type: 'line',
-    data: this.generateChart()
-  },
-  {
-    symbol: 'Q4',
-    type: 'line',
-    data: this.generateChart()
-  }
-];
 
 constructor(){}
   
@@ -185,8 +162,9 @@ ngOnInit(): void {
 
   renderPerformanceGrid() {
     this.loading = true;
-    //var fields = ['strCol1','strCol2','strCol3','strCol4', 'strCol5','strCol6','floatCol','strCol7','strCol8'];
-    var fields = ['strCol1','strCol2','strCol3','strCol4', 'strCol5','strCol6','strCol7','strCol8','strCol9'];
+    var fields = ['strCol1','strCol2','strCol3','strCol4', 'strCol5','strCol6','strCol7','strCol8',
+                  'strCol9','strCol10', 'strCol11','strCol12','strCol13','strCol14','strCol15','strCol16',
+                  'strCol17','strCol18','strCol19','strCol20'];
     var grid = document.getElementById('grid1') as any;
     var temp_fromdate =document.getElementById("from-date") as any;
     this.fromDate=temp_fromdate.value;
@@ -194,50 +172,147 @@ ngOnInit(): void {
     this.toDate=temp_todate.value;
     //var signal = document.getElementById('selectSignal') as any;
     var benchMark = document.getElementById('selectBenchMark') as any;
+    var benchMarkFlag : any = benchMark.value;
     var spread = document.getElementById('spread') as any;
-    var flag : boolean = spread.value;
-    console.log("spread= "+spread.value);
+    var spreadFlag : any = spread.value;
+
+    var selectFractile : any = document.getElementById("selectFractile") as any;
+    var fractileSelected : any = selectFractile.value;
+    var userFractileSelected = parseInt(fractileSelected, 10);
+    var userFractileSelected = userFractileSelected+3;
+    //console.log("userFractileSelected="+ userFractileSelected);
+
+    // this.observationArray=[];
+     for(var i=0;i< userFractileSelected-2 ;i++){
+       this.observationArray[i].push(148);      // Make other array also here 
+     }
 
     const performanceGrid = [
-      ["Start Date",this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate],
-      ["End Date",this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate],
-      ["Total Number of Observations","148","148","148","148","148",spread.value,benchMark.value],
-      ["Valid Observations","147","147","147","147","147",spread.value,benchMark.value],
-      ["Cumulative Returns", "0.019","0.037","0.084","0.028","0.035",spread.value,benchMark.value],
-      ["CAGR","0.002","0.003","0.007","0.002","0.003",spread.value,benchMark.value],
-      ["Annualized Volatility","0.013","0.015","0.012","0.013","0.015",spread.value,benchMark.value],
-      ["Information Ratio","0.118","0.204","0.553","0.118","0.204",spread.value,benchMark.value],
-      ["t-stat","0.118","0.204","0.553","0.118","0.204",spread.value,benchMark.value],
-      ["Skewness","0.118","0.204","0.553","0.225","0.204",spread.value,benchMark.value],
-      ["Kurtosis","0.555","0.286","-0.047","1.556","0.411",spread.value,benchMark.value],
-      ["Hit Ratio","0.517","0.469","0.551","0.524","0.49",spread.value,benchMark.value],
-      ["Maximum Drawdown", "-0.032","-0.037","-0.038","-0.03","-0.029",spread.value,benchMark.value],
-      ["Value-at-Risk"," 95%"," -0.01","-0.012","	-0.01","-0.014",spread.value,benchMark.value],
-      ["Expected Shortfall", "95"," -0.01","-0.012","-0.01","-0.014",spread.value,benchMark.value],
-      ["Turnover","1.6","1.6","1.6","1.6","1.6",spread.value,benchMark.value]
+      ["Start Date",this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,
+                    this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate],
+      ["End Date",this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,
+                  this.toDate,this.toDate,,this.toDate,this.toDate],
+     ["Total Observations","147","147","147","147","147",
+                  spread.value,benchMark.value],
+      ["Valid Observations","147","147","147","147","147",
+                  spread.value,benchMark.value],
+      ["Cumulative Returns","0.019","0.037","0.84","0.028","0.03","0.524","147","147",spread.value,benchMark.value],
+      ["CAGR","0.002","0.003","0.007","0.002","0.003","0.524","147","147",spread.value,benchMark.value],
+      ["Annualized Volatility","0.013","0.015","0.012","0.013","0.015","0.524",spread.value,benchMark.value],
+      ["Information Ratio","0.118","0.204","0.553","0.118","0.204","0.524",spread.value,benchMark.value],
+      ["t-stat","0.118","0.204","0.553","0.118","0.204","0.524",spread.value,benchMark.value],
+      ["Skewness","0.118","0.204","0.553","0.225","0.204","0.524",spread.value,benchMark.value],
+      ["Kurtosis","0.555","0.286","-0.047","1.556","0.411","0.524",spread.value,benchMark.value],
+      ["Hit Ratio","0.517","0.469","0.551","0.524","0.49","0.524",spread.value,benchMark.value],
+      ["Maximum Drawdown", "-0.032","-0.037","-0.038","-0.03","-0.029","0.524",spread.value,benchMark.value],
+      ["Value-at-Risk"," 95%","-0.01","-0.012","-0.01","-0.014","0.524",spread.value,benchMark.value],
+      ["Expected Shortfall", "95","-0.01","-0.012","-0.01","-0.014","-0.015",spread.value,benchMark.value],
+      ["Turnover","1.6","1.3","1.6","1.6","1.7","1.8",spread.value,benchMark.value]
       ] as any;
+
+
+     if(spreadFlag =="True" && benchMarkFlag!=='null'){
+      this.columnArray=[];
+      for(var i=1;i<=userFractileSelected;i++){
+        if(i==1){
+          this.columnArray.push({title:'Metric',field :'strCol'+i.toString() })
+        }
+        else if(i==userFractileSelected){
+          this.columnArray.push({title: 'BenchMark' ,field :'strCol'+i.toString()})
+        } 
+        else if(i==userFractileSelected-1){
+          this.columnArray.push({title: 'Long/Short Spread' ,field :'strCol'+i.toString()})
+        } 
+        else{
+          this.columnArray.push({title:'Q'+(i-1).toString(),field :'strCol'+i.toString()})
+        }
+      }
+      grid.config = {
+        rowHeight: 40,
+      sorting: {
+      sortableColumns: true
+      },
+      columns:this.columnArray,
+    dataModel: { 
+    fields: fields,
+    data : performanceGrid
+    }}; 
+   }
+
+   else if(spreadFlag=="True" && benchMarkFlag==='null'){
+        this.columnArray=[];
+      for(var i=1;i<=userFractileSelected-1;i++ ){
+        if(i==1){
+          this.columnArray.push({title:'Metric',field :'strCol'+i.toString()})
+        }
+        else if(i==userFractileSelected-1){
+          this.columnArray.push({title: 'Long/Short Spread' ,field :'strCol'+i.toString()})
+        } 
+        else{
+          this.columnArray.push({title:'Q'+(i-1).toString(),field :'strCol'+i.toString()})
+        }
+      }
+    grid.config = {
+      rowHeight: 40,
+    sorting: {
+    sortableColumns: true
+    },
+    columns: this.columnArray,
+  dataModel: { 
+  fields: fields,
+  data : performanceGrid
+  }}; 
+  }
+
+  else if (spreadFlag=="False" && benchMarkFlag!=='null'){
+    this.columnArray=[];
+      for(var i=1;i<=userFractileSelected-1;i++ ){
+        if(i==1){
+          this.columnArray.push({title:'Metric',field :'strCol'+i.toString()})
+        }
+        else if(i==userFractileSelected-1){
+          this.columnArray.push({title: 'BenchMark' ,field :'strCol'+i.toString()})
+        } 
+        else{
+          this.columnArray.push({title:'Q'+(i-1).toString(),field :'strCol'+i.toString()})
+        }
+      }
 
     grid.config = {
       rowHeight: 40,
     sorting: {
     sortableColumns: true
     },
-    columns: [
-    { title: 'Metric', field: fields[0] , width: 250},
-    { title: 'Q1', field: fields[1], width : 100 },
-    { title: 'Q2', field: fields[2] },
-    { title: 'Q3', field: fields[3] },
-    { title: 'Q4', field: fields[4] },
-    { title: 'Q5', field: fields[5] , visible :true},
-    // <input [attr.disabled]="disabled ? '' : null"/>
-    { title: 'Long/Short Spread', field: fields[6], visible : [spread.value]="true ? 'true' : 'false'"},
-    { title: 'BenchMark', field: fields[7] , visible : flag },
-  ],
+    columns: this.columnArray,
   dataModel: { 
   fields: fields,
   data : performanceGrid
-   } };
+  }}; 
+  }
+
+  else if (spreadFlag=="False" && benchMarkFlag==='null'){
+    this.columnArray=[];
+      for(var i=1;i<=userFractileSelected-2;i++ ){
+        if(i==1){
+          this.columnArray.push({title:'Metric',field :'strCol'+i.toString()})
+        }
+        else{
+          this.columnArray.push({title:'Q'+(i-1).toString(),field :'strCol'+i.toString()})
+        }
+      }
+    grid.config = {
+      rowHeight: 40,
+    sorting: {
+    sortableColumns: true
+    },
+    columns: this.columnArray,
+  dataModel: { 
+  fields: fields,
+  data : performanceGrid
+  }}; 
+  }
 }
+  
 
 
 displayMultipleChart(fromDate1 : string, toDate1 : string): any {
@@ -245,48 +320,13 @@ displayMultipleChart(fromDate1 : string, toDate1 : string): any {
      const endDate1 = new Date(toDate1);
      var multiplechart : any = document.getElementById("multiple") as any;
      var selectFractile : any = document.getElementById("selectFractile") as any;
-    var temp_series :any = [];
-    var fractile_data : any =[
-      {
-        symbol: 'Q1',
-        type: 'line',
-        data : this.generateChart()
-      },
-      {
-        symbol: 'Q2',
-        type: 'line',
-        data: this.generateChart()
-      },
-      {
-        symbol: 'Q3',
-        type: 'line',
-        data: this.generateChart()
-      },
-      {
-        symbol: 'Q4',
-        type: 'line',
-        data: this.generateChart()
-      },
-      {
-        symbol: 'Q5',
-        type: 'line',
-        data: this.generateChart()
-      },
-      {
-        symbol: 'Long/Short Spread',
-        type: 'line',
-        data: this.generateChart()
-      },
-      {
-        symbol: 'BenchMark',
-        type: 'line',
-        data: this.generateChart()
-      },
-    ];
+     var temp_series :any = [];
     this.start_Date = startDate1;
     this.end_Date = endDate1;
-     for(var i= 0;i< selectFractile.value;i++){
-       temp_series.push(fractile_data[i]);
+    
+     for(var i= 0;i< selectFractile.value ;i++){
+      console.log("Inside Loop ");
+       temp_series.push({symbol :'Q'+(i-1).toString(),type: 'line',data: this.generateChart()});
      }
     multiplechart.config = {
       options: {
@@ -318,5 +358,4 @@ displayMultipleChart(fromDate1 : string, toDate1 : string): any {
     }
     return ret;
   }
-
 }
