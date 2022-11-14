a0b81c0a5cf7a3f78eaa9d32d0d04fe07c31ad29
Santosh <santosh.kumarnayak.rdp@lseg.com>
2022-11-14 22:51:27 +0530

    monday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -92,12 +92,15 @@ json_Data : any = [];
 json_Data1 : any = [];
 data : any = [];
 columnNames : Array<any> = [];
+gridColumns : Array<any> = [];
 rowArray : Array<any> = [];
 rowArrayFlag : Array<any> = [];
 rowArrayData : Array<any> = [];
 start_Date :any = '2022-01-01';          
 end_Date : any = '2022-12-31';
 entries : any = [];
+perfEntries : any =[];
+gridPerfObject : any =[];
 keys : any = [];
 arr2 :any =[];
 bmObject : any = [];
@@ -105,6 +108,7 @@ priceObject : any = [];
 weiObject : any = [];
 bmArray :any = [];
 priceArray : any = [];
+
 Q1 : any = [];
 LS : any = [];
 
@@ -113,14 +117,28 @@ valArr : any = [];
 dateArr1 : any = [];
 valArr1 : any = [];
 tt : any =[];
+portfolio : any =[];
+totalObservations : any = [];
+validObservations : any = [];
+cumulativeReturns : any = [];
+CAGR : any = [];
+AnnualizedVolatility : any = [];
+InformationRatio : any = [];
+tstat :any =[];
+skewness : any =[];
+kurtosis : any =[];
+hitRatio : any =[];
+maxDrawdown : any = [];
+
+
 
 constructor(){}
   
 ngOnInit(): void {  
     this.loading = false;
     this.retrieveBenchMarks();
+    this.retrievePerformance();
     this.getBMData(this.entries);            // Loading data in Array 
-
     this.onSubmit();
     this.initializeUniverseComboBox();   
     this.initializeSignalComboBox();  
@@ -218,7 +236,7 @@ ngOnInit(): void {
     var temp_todate=document.getElementById("to-date") as any;
     this.toDate=temp_todate.value;
     this.displayMultipleChart(this.fromDate,this.toDate);
-    this.renderPerformanceGrid();
+    this.renderPerformanceGrid(this.perfEntries);
   }
 
   initializeLayoutEvent(): void {
@@ -270,7 +288,8 @@ ngOnInit(): void {
   }
 
 
-renderPerformanceGrid() {
+renderPerformanceGrid(perfEntries : Object): void {
+  console.log("Inside renderPerformanceGrid");
   var fields = ['strCol1','strCol2','strCol3','strCol4', 'strCol5','strCol6','strCol7','strCol8',
                    'strCol9','strCol10', 'strCol11','strCol12','strCol13','strCol14','strCol15','strCol16',
                     'strCol17','strCol18','strCol19','strCol20'
@@ -289,44 +308,111 @@ renderPerformanceGrid() {
   var userFractileSelected = parseInt(fractileSelected, 10);
   var userFractileSelected = userFractileSelected+2;
 
+  for (const [key, value] of Object.entries(perfEntries)) {
+      //console.log(`${key}  ${value}`); 
+      if (typeof value=='string') {
+      if(key=='performance'){
+        this.gridPerfObject = JSON.parse(value);          
+      }
+    }
+  }
+  //console.log("Grid Keys=" + Object.keys(this.gridPerfObject));
+  //console.log("Grid Keys2 = " + (JSON.stringify(this.gridPerfObject)));
+  this.portfolio = [];
+  this.totalObservations = [];
+  this.validObservations = [];
+  this.cumulativeReturns = [];
+  this.CAGR = [];
+  this.AnnualizedVolatility = [];
+  this.InformationRatio = [];
+  this.tstat = [];
+  this.skewness = [];
+  this.kurtosis = [];
+  this.hitRatio = [];
+  this.maxDrawdown = [];
+  
+
+
+  this.gridPerfObject.forEach(item => {
+    for (let key in item) {
+      //console.log(`${key} : ${item[key]}`)
+      //this.gridColumns.push(key,item[key]);  
+      
+      if(key=='portfolio'){
+        this.portfolio.push(item[key]);
+      }
+      if(key=='Total Number of Observations'){
+        this.totalObservations.push(item[key]);
+      }
+      if(key=='Valid Observations'){
+        this.validObservations.push(item[key]);
+      }
+      if(key=='Cumulative Return'){
+        this.cumulativeReturns.push(item[key]);
+      }
+      if(key=='CAGR'){
+        this.CAGR.push(item[key]);
+      }
+      if(key=='Annualized Volatility'){
+        this.AnnualizedVolatility.push(item[key]);
+      }
+      if(key=='Information Ratio'){
+        this.InformationRatio.push(item[key]);
+      }
+      if(key=='t-stat'){                                 
+        this.tstat.push(item[key]);
+      }
+      if(key=='Skewness'){                                // 
+        this.skewness.push(item[key]);
+      }
+      if(key=='Kurtosis'){                                // 
+        this.kurtosis.push(item[key]);
+      }
+      if(key=='Hit Ratio'){                                // 
+        this.hitRatio.push(item[key]);
+      }
+      if(key=='Maximum Drawdown'){                                // 
+        this.maxDrawdown.push(item[key]);
+      }
+
+
+    }
+  })
+
+    console.log("portfolio = " + this.portfolio);
+    console.log("total Observations = " + this.totalObservations);
+    console.log("valid Observations = " + this.validObservations);
+    console.log("valid cumulativ  = " + this.cumulativeReturns);
+    console.log("valid cagr  = " + this.CAGR);
+    console.log("valid vola = " + this.AnnualizedVolatility);
+    console.log("information Ratio = " + this.InformationRatio);
+
+  
+  this.gridColumns=[];
   this.columnNames=[];
   this.columnNames.push({title:'Metric', field :fields[0] , width : 150})
-  this.columnNames.push({title:'Total Observations',field :fields[1]})
-  this.columnNames.push({title:'Valid Observations',field :fields[2]})
-  this.columnNames.push({title:'Cumulative Returns',field :fields[3]})
-  this.columnNames.push({title:'CAGR',field :fields[4]})
-  this.columnNames.push({title:'Annualized Volatility',field :fields[5]})
-  this.columnNames.push({title:'Information Ratio',field :fields[6]})
-  this.columnNames.push({title:'t-stat',field :fields[7]})
-  this.columnNames.push({title:'Skewness',field :fields[8]})
-  this.columnNames.push({title:'Kurtosis',field :fields[9]})
-  this.columnNames.push({title:'Hit Ratio',field :fields[10]})
-  this.columnNames.push({title:'Maximum Drawdown',field :fields[11]})
-  //this.columnNames.push({title:'Value-at-Risk 95%',field :fields[12]})
-  //this.columnNames.push({title:'Expected Shortfall 95',field :fields[13]})
-  //this.columnNames.push({title:'Turnover',field :fields[14]})
-
+  this.columnNames.push({title:'Portfolio', field :fields[1] })
+  this.columnNames.push({title:'Total Observations',field :fields[2]})
+  this.columnNames.push({title:'Valid Observations',field :fields[3]})
+  this.columnNames.push({title:'Cumulative Returns',field :fields[4]})
+  this.columnNames.push({title:'CAGR',field :fields[5]})
+  this.columnNames.push({title:'Annualized Volatility',field :fields[6]})
+  this.columnNames.push({title:'Information Ratio',field :fields[7]})
+  this.columnNames.push({title:'t-stat',field :fields[8]})
+  this.columnNames.push({title:'Skewness',field :fields[9]})
+  this.columnNames.push({title:'Kurtosis',field :fields[10]})
+  this.columnNames.push({title:'Hit Ratio',field :fields[11]})
+  this.columnNames.push({title:'Maximum Drawdown',field :fields[12]})
+  
 
   this.rowArray=[
-    ["Start Date",this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,
-                     this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate],
-    ["End Date",this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,
-                   this.toDate,this.toDate,this.toDate,this.toDate,this.toDate],
-    ["Q1","800","34","000","98722","67822","987","56","65","111","78","234","234"],
-    ["Q2","10332","1222","3422","9800","987","67822","45","678","678","009","007"],
-    ["Q3","132","1222","3422","9800","987","67822","54","678","1222","9800","987"],
-    ["Q4","132","1222","3422","9800","987","67822","54","678","378","34","500"],
-    ["Q5","800","34","000","98722","67822","987","56","65","111","987","009"],
-    ["Q6","10332","1222","3422","9800","987","567","45","678","678","009","007"],
-    ["Q7","132","1222","3422","1200","987","678","54","678","4567","9800","987"],
-    ["Q8","132","234","3422","009","987","888","54","678","55","345","009"],
-    ["Q9","132","389","3422","109","973","888","54","678","222","88","111"],
-    ["Q10","132","389","3422","109","973","999","54","678","55","88","99"],
-    ["Q11","132","389","3422","109","973","99","54","678","222","888","99"],
-    ["Q12","99","389","3422","109","973","333","54","678","222","888","99"],
-    ["Q13","99","99","99","109","973","555","54","678","55","66","888"],
-    ["Q14","99","389","3422","109","973","99","54","678","222","111","99"],
-    ["Q15","132","389","3422","109","973","99","54","678","55","111","15511"]
+    ["Start Date",this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate,this.fromDate],
+    ["End Date",this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate,this.toDate],
+    ["Q1",this.portfolio[0],this.totalObservations[0],this.validObservations[0],this.cumulativeReturns[0],this.CAGR[0],this.AnnualizedVolatility[0],this.InformationRatio[0],this.tstat[0],this.skewness[0],this.kurtosis[0],this.hitRatio[0],this.maxDrawdown[0],"234","234"],
+    ["Q2",this.portfolio[1],this.totalObservations[1],this.validObservations[1],this.cumulativeReturns[1],this.CAGR[1],this.AnnualizedVolatility[1],this.InformationRatio[1],this.tstat[1],this.skewness[1],this.kurtosis[1],this.hitRatio[1],this.maxDrawdown[1],"007"],
+    ["Q3",this.portfolio[2],this.totalObservations[2],this.validObservations[2],this.cumulativeReturns[2],this.CAGR[2],this.AnnualizedVolatility[2],this.InformationRatio[2],this.tstat[2],this.skewness[2],this.kurtosis[2],this.hitRatio[2],this.maxDrawdown[2],"9800","987"],
+    ["Q4",this.portfolio[3],this.totalObservations[3],this.validObservations[3],this.cumulativeReturns[3],this.CAGR[3],this.AnnualizedVolatility[3],this.InformationRatio[3],this.tstat[3],this.skewness[3],this.kurtosis[3],this.hitRatio[3],this.maxDrawdown[3],"34","500"],
+    ["Q5",this.portfolio[4],this.totalObservations[4],this.validObservations[4],this.cumulativeReturns[4],this.CAGR[4],this.AnnualizedVolatility[4],this.InformationRatio[4],this.tstat[4],this.skewness[4],this.kurtosis[4],this.hitRatio[4],this.maxDrawdown[4],"987","009"],
   ];
 
   this.rowArrayFlag=[
@@ -335,6 +421,7 @@ renderPerformanceGrid() {
   ];
 
   this.rowArrayData=[];
+
   for(var i=0; i<userFractileSelected ;i++){
     this.rowArrayData.push(this.rowArray[i]);
   }
@@ -479,6 +566,14 @@ generateChart() {
       .then((response) => response.json())
       .then((data) => {
         return this.getBMData(data);
+    });
+  } 
+
+  retrievePerformance() : any{
+    return fetch(`assets/santosh.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        return this.renderPerformanceGrid(data);
     });
   } 
 
